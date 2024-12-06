8f12fc55194aad369d4dcad02d888bdb461b3580
nishanth <n.ganesh@everteam.com>
2024-12-06 23:58:47 +0530

    Fixed swggerdef url path issue.

--- src/api/routes/v1/docs.route.js
+++ src/api/routes/v1/docs.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDefinition = require('../../docs/swaggerDef');
+const swaggerDefinition = require(__dirname.split('api')[0] + 'api\\docs\\swaggerDef');
 
 const router = express.Router();
 
